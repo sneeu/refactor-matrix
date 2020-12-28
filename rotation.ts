06578c758a6d9d3059d18b29c3b6c3de9c8e7de2
John Sutherland <john@starstock.com>
2020-12-28 15:26:23 +0000

    Make the similarities even more obvious

--- rotation.ts
+++ rotation.ts
@@ -5,22 +5,28 @@ export type Point = {
 };
 
 export const rotateX = (point: Point, theta: number): Point => {
-  const x_ = point.x * 1;
-  const y_ = point.y * Math.cos(theta) - point.z * Math.sin(theta);
-  const z_ = point.y * Math.sin(theta) + point.z * Math.cos(theta);
+  const x_ = point.x * 1 + point.y * 0 + point.z * 0;
+  const y_ =
+    point.x * 0 + point.y * Math.cos(theta) - point.z * Math.sin(theta);
+  const z_ =
+    point.x * 0 + point.y * Math.sin(theta) + point.z * Math.cos(theta);
   return { x: x_, y: y_, z: z_ };
 };
 
 export const rotateY = (point: Point, theta: number): Point => {
-  const x_ = point.x * Math.cos(theta) + point.z * Math.sin(theta);
-  const y_ = point.y * 1;
-  const z_ = -point.x * Math.sin(theta) + point.z * Math.cos(theta);
+  const x_ =
+    point.x * Math.cos(theta) + point.y * 0 + point.z * Math.sin(theta);
+  const y_ = point.x * 0 + point.y * 1 + point.z * 0;
+  const z_ =
+    -point.x * Math.sin(theta) + point.y * 0 + point.z * Math.cos(theta);
   return { x: x_, y: y_, z: z_ };
 };
 
 export const rotateZ = (point: Point, theta: number): Point => {
-  const x_ = point.x * Math.cos(theta) - point.y * Math.sin(theta);
-  const y_ = point.x * Math.sin(theta) + point.y * Math.cos(theta);
-  const z_ = point.z * 1;
+  const x_ =
+    point.x * Math.cos(theta) - point.y * Math.sin(theta) + point.z * 0;
+  const y_ =
+    point.x * Math.sin(theta) + point.y * Math.cos(theta) + point.z * 0;
+  const z_ = point.x * 0 + point.y * 0 + point.z * 1;
   return { x: x_, y: y_, z: z_ };
 };
