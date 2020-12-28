82b181b22f3bd0b67d5eb7cbf2576d08bd6b2405
John Sutherland <john@starstock.com>
2020-12-28 15:15:52 +0000

    Add rotateY & rotateZ

--- rotation.test.ts
+++ rotation.test.ts
@@ -1,16 +1,73 @@
-import { rotateX, Point } from "./rotation";
+import { rotateX, rotateY, rotateZ } from "./rotation";
 
-test("rotates points along the X axis by nothing", () => {
-  expect(rotateX({ x: 0, y: 0, z: 0 }, 90)).toStrictEqual({ x: 0, y: 0, z: 0 });
-  expect(rotateX({ x: 1, y: 0, z: 0 }, 90)).toStrictEqual({ x: 1, y: 0, z: 0 });
-  expect(rotateX({ x: -2, y: 0, z: 0 }, 90)).toStrictEqual({
-    x: -2,
-    y: 0,
-    z: 0,
+describe.each([
+  // On the X axis
+  [{ x: 0, y: 0, z: 0 }, Math.PI / 2, { x: 0, y: -0, z: 0 }],
+  [{ x: 1, y: 0, z: 0 }, Math.PI / 2, { x: 1, y: -0, z: 0 }],
+  [{ x: -2, y: 0, z: 0 }, Math.PI / 2, { x: -2, y: -0, z: 0 }],
+
+  // By π/2
+  [{ x: 3, y: 11, z: 19 }, Math.PI / 2, { x: 3, y: -19, z: 11 }],
+  [{ x: 5, y: 13, z: 23 }, Math.PI / 2, { x: 5, y: -23, z: 13 }],
+  [{ x: 7, y: 17, z: 27 }, Math.PI / 2, { x: 7, y: -27, z: 17 }],
+
+  // By π
+  [{ x: 3, y: 11, z: 19 }, Math.PI, { x: 3, y: -11, z: -19 }],
+  [{ x: 5, y: 13, z: 23 }, Math.PI, { x: 5, y: -13, z: -23 }],
+  [{ x: 7, y: 17, z: 27 }, Math.PI, { x: 7, y: -17, z: -27 }],
+])("rotateX", (point, theta, expected) => {
+  test("rotates points around the X axis", () => {
+    const rotatedPoint = rotateX(point, theta);
+    expect(rotatedPoint.x).toBeCloseTo(expected.x);
+    expect(rotatedPoint.y).toBeCloseTo(expected.y);
+    expect(rotatedPoint.z).toBeCloseTo(expected.z);
   });
-  expect(rotateX({ x: 103, y: 0, z: 0 }, 90)).toStrictEqual({
-    x: 103,
-    y: 0,
-    z: 0,
+});
+
+describe.each([
+  // On the Y axis
+  [{ x: 0, y: 0, z: 0 }, Math.PI / 2, { x: 0, y: 0, z: 0 }],
+  [{ x: 0, y: 1, z: 0 }, Math.PI / 2, { x: 0, y: 1, z: 0 }],
+  [{ x: 0, y: -2, z: 0 }, Math.PI / 2, { x: 0, y: -2, z: 0 }],
+
+  // By π/2
+  [{ x: 3, y: 11, z: 19 }, Math.PI / 2, { x: 19, y: 11, z: -3 }],
+  [{ x: 5, y: 13, z: 23 }, Math.PI / 2, { x: 23, y: 13, z: -5 }],
+  [{ x: 7, y: 17, z: 27 }, Math.PI / 2, { x: 27, y: 17, z: -7 }],
+
+  // By π
+  [{ x: 3, y: 11, z: 19 }, Math.PI, { x: -3, y: 11, z: -19 }],
+  [{ x: 5, y: 13, z: 23 }, Math.PI, { x: -5, y: 13, z: -23 }],
+  [{ x: 7, y: 17, z: 27 }, Math.PI, { x: -7, y: 17, z: -27 }],
+])("rotateY", (point, theta, expected) => {
+  test("rotates points around the Y axis", () => {
+    const rotatedPoint = rotateY(point, theta);
+    expect(rotatedPoint.x).toBeCloseTo(expected.x);
+    expect(rotatedPoint.y).toBeCloseTo(expected.y);
+    expect(rotatedPoint.z).toBeCloseTo(expected.z);
+  });
+});
+
+describe.each([
+  // On the Z axis
+  [{ x: 0, y: 0, z: 0 }, Math.PI / 2, { x: 0, y: 0, z: 0 }],
+  [{ x: 0, y: 0, z: 1 }, Math.PI / 2, { x: 0, y: 0, z: 1 }],
+  [{ x: 0, y: 0, z: -2 }, Math.PI / 2, { x: 0, y: 0, z: -2 }],
+
+  // By π/2
+  [{ x: 3, y: 11, z: 19 }, Math.PI / 2, { x: -11, y: 3, z: 19 }],
+  [{ x: 5, y: 13, z: 23 }, Math.PI / 2, { x: -13, y: 5, z: 23 }],
+  [{ x: 7, y: 17, z: 27 }, Math.PI / 2, { x: -17, y: 7, z: 27 }],
+
+  // By π
+  [{ x: 3, y: 11, z: 19 }, Math.PI, { x: -3, y: -11, z: 19 }],
+  [{ x: 5, y: 13, z: 23 }, Math.PI, { x: -5, y: -13, z: 23 }],
+  [{ x: 7, y: 17, z: 27 }, Math.PI, { x: -7, y: -17, z: 27 }],
+])("rotateZ", (point, theta, expected) => {
+  test("rotates points around the Z axis", () => {
+    const rotatedPoint = rotateZ(point, theta);
+    expect(rotatedPoint.x).toBeCloseTo(expected.x);
+    expect(rotatedPoint.y).toBeCloseTo(expected.y);
+    expect(rotatedPoint.z).toBeCloseTo(expected.z);
   });
 });
--- rotation.ts
+++ rotation.ts
@@ -1,9 +1,23 @@
 export type Point = {
-  x: Number;
-  y: Number;
-  z: Number;
+  x: number;
+  y: number;
+  z: number;
 };
 
-export const rotateX = (point: Point, _theta: Number): Point => {
-  return point;
+export const rotateX = (point: Point, theta: number): Point => {
+  const y_ = point.y * Math.cos(theta) - point.z * Math.sin(theta);
+  const z_ = point.y * Math.sin(theta) + point.z * Math.cos(theta);
+  return { x: point.x, y: y_, z: z_ };
+};
+
+export const rotateY = (point: Point, theta: number): Point => {
+  const x_ = point.x * Math.cos(theta) + point.z * Math.sin(theta);
+  const z_ = -point.x * Math.sin(theta) + point.z * Math.cos(theta);
+  return { x: x_, y: point.y, z: z_ };
+};
+
+export const rotateZ = (point: Point, theta: number): Point => {
+  const x_ = point.x * Math.cos(theta) - point.y * Math.sin(theta);
+  const y_ = point.x * Math.sin(theta) + point.y * Math.cos(theta);
+  return { x: x_, y: y_, z: point.z };
 };
