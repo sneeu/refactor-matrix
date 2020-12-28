745f28a8b5d99f42461da0092010318a1e03eb2c
John Sutherland <john@starstock.com>
2020-12-28 15:21:37 +0000

    Make the similarities more obvious

--- rotation.ts
+++ rotation.ts
@@ -5,19 +5,22 @@ export type Point = {
 };
 
 export const rotateX = (point: Point, theta: number): Point => {
+  const x_ = point.x * 1;
   const y_ = point.y * Math.cos(theta) - point.z * Math.sin(theta);
   const z_ = point.y * Math.sin(theta) + point.z * Math.cos(theta);
-  return { x: point.x, y: y_, z: z_ };
+  return { x: x_, y: y_, z: z_ };
 };
 
 export const rotateY = (point: Point, theta: number): Point => {
   const x_ = point.x * Math.cos(theta) + point.z * Math.sin(theta);
+  const y_ = point.y * 1;
   const z_ = -point.x * Math.sin(theta) + point.z * Math.cos(theta);
-  return { x: x_, y: point.y, z: z_ };
+  return { x: x_, y: y_, z: z_ };
 };
 
 export const rotateZ = (point: Point, theta: number): Point => {
   const x_ = point.x * Math.cos(theta) - point.y * Math.sin(theta);
   const y_ = point.x * Math.sin(theta) + point.y * Math.cos(theta);
-  return { x: x_, y: y_, z: point.z };
+  const z_ = point.z * 1;
+  return { x: x_, y: y_, z: z_ };
 };
