b297a1df8d8ebac03f28d01c3794b9f6b26fc1d8
John Sutherland <john@starstock.com>
2020-12-28 15:43:36 +0000

    Factor out rotation

--- rotation.ts
+++ rotation.ts
@@ -4,29 +4,49 @@ export type Point = {
   z: number;
 };
 
-export const rotateX = (point: Point, theta: number): Point => {
-  const x_ = point.x * 1 + point.y * 0 + point.z * 0;
+export const rotate = (point: Point, transformMatrix: number[][]): Point => {
+  const x_ =
+    transformMatrix[0][0] * point.x +
+    transformMatrix[0][1] * point.y +
+    transformMatrix[0][2] * point.z;
   const y_ =
-    point.x * 0 + point.y * Math.cos(theta) - point.z * Math.sin(theta);
+    transformMatrix[1][0] * point.x +
+    transformMatrix[1][1] * point.y +
+    transformMatrix[1][2] * point.z;
   const z_ =
-    point.x * 0 + point.y * Math.sin(theta) + point.z * Math.cos(theta);
+    transformMatrix[2][0] * point.x +
+    transformMatrix[2][1] * point.y +
+    transformMatrix[2][2] * point.z;
+
   return { x: x_, y: y_, z: z_ };
+};
+
+export const rotateX = (point: Point, theta: number): Point => {
+  const s = Math.sin(theta);
+  const c = Math.cos(theta);
+  return rotate(point, [
+    [1, 0, 0],
+    [0, c, -s],
+    [0, s, c],
+  ]);
 };
 
 export const rotateY = (point: Point, theta: number): Point => {
-  const x_ =
-    point.x * Math.cos(theta) + point.y * 0 + point.z * Math.sin(theta);
-  const y_ = point.x * 0 + point.y * 1 + point.z * 0;
-  const z_ =
-    -point.x * Math.sin(theta) + point.y * 0 + point.z * Math.cos(theta);
-  return { x: x_, y: y_, z: z_ };
+  const s = Math.sin(theta);
+  const c = Math.cos(theta);
+  return rotate(point, [
+    [c, 0, s],
+    [0, 1, 0],
+    [-s, 0, c],
+  ]);
 };
 
 export const rotateZ = (point: Point, theta: number): Point => {
-  const x_ =
-    point.x * Math.cos(theta) - point.y * Math.sin(theta) + point.z * 0;
-  const y_ =
-    point.x * Math.sin(theta) + point.y * Math.cos(theta) + point.z * 0;
-  const z_ = point.x * 0 + point.y * 0 + point.z * 1;
-  return { x: x_, y: y_, z: z_ };
+  const s = Math.sin(theta);
+  const c = Math.cos(theta);
+  return rotate(point, [
+    [c, -s, 0],
+    [s, c, 0],
+    [0, 0, 1],
+  ]);
 };
